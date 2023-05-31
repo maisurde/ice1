30acb729e1ec5c8fc8d2cac766e3725f295bf678
User <User@10.48.30.101>
2023-05-31 14:27:59 -0400

    ice1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,22 +10,72 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
  */
-public class CardTrick {
+import java.util.Random;
+import java.util.Scanner;
+
+public class CardTrick 
+{
     
     public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
+        Card[] magicHand = new Card[8];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(generateRandomNumber());
+            c.setSuit(Card.SUITS[generateRandomNumber(0, 3)]);
+            magicHand[i] = c;
         }
+
+        Scanner kb = new Scanner(System.in);
+        System.out.print("Enter the value of your card: ");
+        int value = kb.nextInt();
+        System.out.print("Enter the suit of your card (0-3): ");
+        int suitIndex = kb.nextInt();
+
+        Card userCard = new Card();
+        userCard.setValue(value);
+        userCard.setSuit(Card.SUITS[suitIndex]);
+
+        boolean found = false;
+        for (Card card : magicHand) 
+        {
+            if (card.getValue() == userCard.getValue() && card.getSuit().equals(userCard.getSuit())) 
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) 
+        {
+            System.out.println("Your card is in the magic hand!");
+        } 
+        
+        else 
+        {
+            System.out.println("Your card is not in the magic hand.");
+        }
+    }
+
+    private static int generateRandomNumber() 
+    {
+        Random random = new Random();
+        return random.nextInt(13) + 1;
+    }
+
+    private static int generateRandomNumber(int min, int max) 
+    {
+        Random random = new Random();
+        return random.nextInt(max - min + 1) + min;
+    }
+
+}
+        //c.setValue(insert call to random number generator here)
+            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-    }
-    
-}
